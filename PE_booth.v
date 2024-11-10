182566eaf690f607c8f2ccb06b1dc1e10b117453
dandekarsneha07 <dandekarsneha07@gmail.com>
2024-11-10 10:38:44 +0530

    Update PE_booth.v

--- PE_booth.v
+++ PE_booth.v
@@ -61,17 +61,16 @@ module booth_multiplier #(parameter REG_WIDTH = 8, OUT_WIDTH = REG_WIDTH*2)(
         partial_sum = 0;
 
         // Radix-4 Booth Encoding Loop
-        for (i = 0; i < REG_WIDTH / 2; i = i + 1) begin
+       for (i = 0; i < REG_WIDTH / 2; i = i + 1) begin
             case (booth_reg[2:0])
                 3'b000, 3'b111: partial_sum = partial_sum;            // No addition
-                3'b001, 3'b010: partial_sum = partial_sum + (A << (2 * i)); // Add A shifted by 2*i
-                3'b011:          partial_sum = partial_sum + (A << (2 * i + 1)); // Add 2 * A shifted
-                3'b100:          partial_sum = partial_sum - (A << (2 * i + 1)); // Subtract 2 * A shifted
-                3'b101, 3'b110:  partial_sum = partial_sum - (A << (2 * i)); // Subtract A shifted
+                3'b001, 3'b010: partial_sum = partial_sum + (A << (i<<1)); // Add A shifted by 2*i
+                3'b011:          partial_sum = partial_sum + (A << ((i<<1) + 1)); // Add 2 * A shifted
+                3'b100:          partial_sum = partial_sum - (A << ((i<<1)+ 1)); // Subtract 2 * A shifted
+                3'b101, 3'b110:  partial_sum = partial_sum - (A << (i<<1)); // Subtract A shifted
             endcase
             booth_reg = booth_reg >> 2; // Shift booth_reg by 2 for next pair
         end
-        
         // Assign the final product
         P = partial_sum;
     end
